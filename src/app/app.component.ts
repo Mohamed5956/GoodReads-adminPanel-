7f8adf7b743b44744011f94eb6045e826384a9f6
Mohammed <mohammedadell301@gmail.com>
2023-03-04 13:02:30 +0200

    design for admin panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'goodreedsAdmin';
+  sideBarOpen = true;
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
