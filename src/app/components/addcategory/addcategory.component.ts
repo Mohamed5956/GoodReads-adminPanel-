62d50276a8ef5a1eb4cc50531581bf354ddc5067
Mohammed <mohammedadell301@gmail.com>
2023-03-05 13:15:43 +0200

    add category

--- src/app/components/addcategory/addcategory.component.ts
+++ src/app/components/addcategory/addcategory.component.ts
@@ -1,35 +1,48 @@
 
-  import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
-  import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
-  import { Icategory } from 'src/app/models/icategory';
-  import { CategoryService } from 'src/app/services/category.service';
+import { Icategory } from 'src/app/models/icategory';
+import { CategoryService } from 'src/app/services/category.service';
 
-  import { CategoryComponent } from '../category/category.component';
+import { CategoryComponent } from '../category/category.component';
 
-  @Component({
-    selector: 'app-addcategory',
-    templateUrl: './addcategory.component.html',
-    styleUrls: ['./addcategory.component.css']
-  })
-  export class AddcategoryComponent {
-    newCat: Icategory = {}
-    constructor(
-      public dialogRef: MatDialogRef<CategoryComponent>,
-      private categorySerivce: CategoryService) {
-    }
-    saveData() {
-      console.log("newCat", this.newCat)
-      this.categorySerivce.addCategory(this.newCat).subscribe(response => {
-        console.log('Data saved successfully!', response);
-        this.dialogRef.close();
-      }, error => {
-        console.log('Error saving data:', error);
-      });
-    }
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
-    closeDialog() {
-      this.dialogRef.close();
-    }
+
+@Component({
+  selector: 'app-addcategory',
+  templateUrl: './addcategory.component.html',
+  styleUrls: ['./addcategory.component.css']
+})
+export class AddcategoryComponent {
+  newCat: Icategory = {}
+  categoryForm: FormGroup
+  name: boolean = false;
+  constructor(
+    public dialogRef: MatDialogRef<CategoryComponent>,
+    private categorySerivce: CategoryService,
+    private fb: FormBuilder,
+  ) {
+    this.categoryForm = this.fb.group({
+      name: ['', Validators.required]
+    })
   }
+  saveData() {
+    this.newCat = {
+      name: this.categoryForm.value.name
+    };
+    console.log(this.newCat);
+    this.categorySerivce.addCategory(this.newCat).subscribe(response => {
+      console.log('Data saved successfully!', response);
+      this.dialogRef.close();
+    }, error => {
+      console.log('Error saving data:', error);
+    });
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+}
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -14,19 +14,7 @@ export class CategoryComponent {
   openDialog() {
     const dialogRef = this.dialog.open(AddcategoryComponent, {
       width: '400px',
-      data: { name: '' }
     });
 
-    // dialogRef.afterClosed().subscribe(result => {
-    //   if (result) {
-    //     this.categoryService.addCategory(result).subscribe(() => {
-    //       // handle successful book addition
-    //       console.log(result);
-    //     }, (err) => {
-    //       // handle failed book addition
-    //       console.log(err)
-    //     });
-    //   }
-    // });
   }
 }
